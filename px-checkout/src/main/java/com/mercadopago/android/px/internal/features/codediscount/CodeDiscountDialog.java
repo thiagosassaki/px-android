7c649a506d47127cc73e40b29be89c74adeb17a6
matiasromar <matias.romar@mercadolibre.com>
2018-09-10 16:23:20 -0300

    fixes ux

--- px-checkout/src/main/java/com/mercadopago/android/px/internal/features/codediscount/CodeDiscountDialog.java
+++ px-checkout/src/main/java/com/mercadopago/android/px/internal/features/codediscount/CodeDiscountDialog.java
@@ -6,6 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
 import com.mercadolibre.android.ui.widgets.MeliButton;
