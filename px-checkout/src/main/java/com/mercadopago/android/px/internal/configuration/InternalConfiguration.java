6c5024e0065f570685cbc165213050de8a2e24b5
matiasromar <matias.romar@mercadolibre.com>
2018-09-11 16:15:03 -0300

    deleted builder

--- px-checkout/src/main/java/com/mercadopago/android/px/internal/configuration/InternalConfiguration.java
+++ px-checkout/src/main/java/com/mercadopago/android/px/internal/configuration/InternalConfiguration.java
@@ -5,37 +5,23 @@ import java.io.Serializable;
 /**
  * Internal configuration provides support to money in for custom checkout functionality/configure special behaviour
  * when checkout is running.
+ *
+ * @see <a href="https://github.com/mercadolibre/fury_moneyin-android">https://github.com/mercadolibre/fury_moneyin-android</a>
  */
 @SuppressWarnings("unused")
 public class InternalConfiguration implements Serializable {
 
     private final boolean exitOnPaymentMethodChange;
 
-    /* default */ InternalConfiguration(final Builder builder) {
-        exitOnPaymentMethodChange = builder.exitOnPaymentMethodChange;
+    /**
+     * Enable to do not show payment method selection when
+     * payment method is changed
+     */
+    public InternalConfiguration(final boolean exitOnPaymentMethodChange) {
+        this.exitOnPaymentMethodChange = exitOnPaymentMethodChange;
     }
 
     public boolean shouldExitOnPaymentMethodChange() {
         return exitOnPaymentMethodChange;
-    }
-
-    public static class Builder {
-        /* default */ boolean exitOnPaymentMethodChange = false;
-
-        /**
-         * Enable to do not show payment method selection when
-         * payment method is changed
-         *
-         * @return builder to keep operating
-         */
-        public Builder exitOnPaymentMethodChange() {
-            exitOnPaymentMethodChange = true;
-            return this;
-        }
-
-        public InternalConfiguration build() {
-
-            return new InternalConfiguration(this);
-        }
     }
 }
